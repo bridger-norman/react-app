74f728619cb7005e3544a1ee9ea6dd974837b1a0
bridger-norman <littlebnorm@gmail.com>
2025-06-17 14:52:23 -0600

    basic table for the beginning of my form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import Alert from "./components/Alert";
 // import Button from "./components/Button";
 // import ListGroup from "./components/ListGroup";
+
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 // function App() {
 
@@ -31,24 +33,53 @@
 
 // export default App;
 
-import ExpandableText from "./components/ExpandableText";
+// import ExpandableText from "./components/ExpandableText";
+
+// function App() {
+//   return (
+//     <div>
+//       <ExpandableText maxChars = {10}>
+//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio eveniet
+//         illo, sunt libero qui voluptas magnam cumque itaque recusandae cum
+//         blanditiis consequatur amet eligendi est quidem minus voluptate aut in
+//         sint. Illum, facilis. Harum commodi quod delectus nostrum saepe eaque,
+//         cum obcaecati unde doloremque vero. Veritatis vero perspiciatis est
+//         architecto officia reprehenderit eum, vitae ex expedita adipisci,
+//         voluptatibus rerum sit alias quidem facere repudiandae voluptas nulla
+//         libero quo nesciunt sapiente consectetur! Temporibus aut dolores
+//         reiciendis repellat molestiae nemo ipsam odit ipsa! Eveniet tempore vero
+//         voluptas vitae inventore porro unde dolore totam, vel neque doloremque
+//         tempora accusantium exercitationem placeat! Minima, temporibus.
+//       </ExpandableText>
+//     </div>
+//   );
+// }
+// export default App;
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Groceries", amount: 50, category: "Food" },
+    {
+      id: 2,
+      description: "Electricity Bill",
+      amount: 75,
+      category: "Utilities",
+    },
+    { id: 3, description: "Gym Membership", amount: 30, category: "Health" },
+    {
+      id: 4,
+      description: "Movie Tickets",
+      amount: 20,
+      category: "Entertainment",
+    },
+  ]);
+
   return (
     <div>
-      <ExpandableText maxChars = {10}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio eveniet
-        illo, sunt libero qui voluptas magnam cumque itaque recusandae cum
-        blanditiis consequatur amet eligendi est quidem minus voluptate aut in
-        sint. Illum, facilis. Harum commodi quod delectus nostrum saepe eaque,
-        cum obcaecati unde doloremque vero. Veritatis vero perspiciatis est
-        architecto officia reprehenderit eum, vitae ex expedita adipisci,
-        voluptatibus rerum sit alias quidem facere repudiandae voluptas nulla
-        libero quo nesciunt sapiente consectetur! Temporibus aut dolores
-        reiciendis repellat molestiae nemo ipsam odit ipsa! Eveniet tempore vero
-        voluptas vitae inventore porro unde dolore totam, vel neque doloremque
-        tempora accusantium exercitationem placeat! Minima, temporibus.
-      </ExpandableText>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
